ca5a3d09c3bf9b3f733d4ee07d119f1872c32fa9
Ralf-GA <ralf.it@goldenarrowsolution.com>
2024-06-22 13:14:16 +0800

    added arcade games

--- app/Http/Controllers/SboStgController.php
+++ app/Http/Controllers/SboStgController.php
@@ -10,7 +10,7 @@ class SboStgController extends Controller
 {
     public function migrate()
     {
-        $pos = 1;
+        $pos = 28;
 
         // foreach ($response['game_list'][213] as $game) {
 
@@ -22,11 +22,11 @@ class SboStgController extends Controller
             $requestData[] = [
                 'providerCode' => 'SBO',
                 'providerName' => 'SBO Sportsbook',
-                'gameCode' => $marketType,
+                'gameCode' => $marketType == 'Mini Mines' ? '285' : '286',
                 'gameName' => $marketType,
                 'position' => $pos,
-                'type' => $marketType,
-                'rtp' => 0,
+                'type' => 'arcade',
+                'rtp' => $marketType == 'Mini Mines' ? '97.0' : '97.50',
                 'imageUrl' => '-',
                 'imageAlt' => $marketType,
             ];
@@ -34,14 +34,15 @@ class SboStgController extends Controller
             $pos++;
         }
 
-        // dd($requestData);
+        dd($requestData);
         // }
 
         foreach ($requestData as $data) {
+            dd($data);
             $response = Http::withHeaders([
                 'Authorization' => 'Bearer 1|y3qi97hqjoxMTBI5OsYxn43OPyK3KHiYJIdnxo2V'
             ])->post(env('dummyAPI'), $data);
-            // ])->post(env('ADD_GAME_API_STG', $data);
+            // ])->post(env('ADD_GAME_API_STG', $data));
 
             Log::info(json_encode([
                 'request' => $data,
@@ -53,33 +54,37 @@ class SboStgController extends Controller
     private function marketType()
     {
         return [
-            'ALL',
-            'Handicap',
-            'Odd/Even',
-            'Over/Under',
-            'Correct Score',
-            '1X2',
-            'Total Goal',
-            'First Half Hdp',
-            'First Half 1x2',
-            'First Half O/U',
-            'HT/FT',
-            'Money Line',
-            'First Half O/E',
-            'First Goal/Last Goal',
-            'First Half CS',
-            'Double Chance',
-            'Live Score',
-            'First Half Live Score',
-            'Outright',
-            'Mix Parlay',
-            'In Between',
-            'First Half 1X2 & O/U',
-            'D/C & First Half O/U',
-            '1X2 & O/U',
-            'D/C & O/U',
-            'First Half RCS',
-            'Reverse Correct Score',
+            'Mini Football Strike',
+            'Mini Mines'
         ];
+        // return [
+        //     'ALL',
+        //     'Handicap',
+        //     'Odd/Even',
+        //     'Over/Under',
+        //     'Correct Score',
+        //     '1X2',
+        //     'Total Goal',
+        //     'First Half Hdp',
+        //     'First Half 1x2',
+        //     'First Half O/U',
+        //     'HT/FT',
+        //     'Money Line',
+        //     'First Half O/E',
+        //     'First Goal/Last Goal',
+        //     'First Half CS',
+        //     'Double Chance',
+        //     'Live Score',
+        //     'First Half Live Score',
+        //     'Outright',
+        //     'Mix Parlay',
+        //     'In Between',
+        //     'First Half 1X2 & O/U',
+        //     'D/C & First Half O/U',
+        //     '1X2 & O/U',
+        //     'D/C & O/U',
+        //     'First Half RCS',
+        //     'Reverse Correct Score',
+        // ];
     }
 }
